0d984b25a24bd9883b88f4b6339243c1584269aa
nxstray <afwanapriansyah@gmail.com>
2026-09-04 01:14:25 +0700

    feat: add EyeOffIcon component

---
+++ src/components/icons/EyeOffIcon.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg :width="size" :height="size" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+    <path d="M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-3.35 2.9A9.12 9.12 0 0 1 12 20c-7 0-11-8-11-8a18.5 18.5 0 0 1 4.22-5.94" />
+    <path d="M14.12 14.12a3 3 0 1 1-4.24-4.24" />
+    <line x1="1" y1="1" x2="23" y2="23" />
+  </svg>
+</template>
+
+<script setup>
+defineProps({ size: { type: [Number, String], default: 16 } })
+</script>
