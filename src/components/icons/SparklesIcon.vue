c688b939f209a5dec0a5117021faa680e96ff37b
nxstray <afwanapriansyah@gmail.com>
2026-09-04 01:14:27 +0700

    feat: add SparklesIcon component

---
+++ src/components/icons/SparklesIcon.vue
@@ -0,0 +1,8 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" :width="size" :height="size" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" :class="className">
+    <path d="M12 3l1.88 5.76a1 1 0 0 0 .95.69h6.06l-4.9 3.56a1 1 0 0 0-.36 1.12L17.5 20l-4.9-3.56a1 1 0 0 0-1.18 0L6.5 20l1.87-5.87a1 1 0 0 0-.36-1.12L3.11 9.45h6.06a1 1 0 0 0 .95-.69L12 3z"/>
+  </svg>
+</template>
+<script setup>
+defineProps({ size: { type: [String, Number], default: 24 }, className: { type: String, default: '' } })
+</script>
