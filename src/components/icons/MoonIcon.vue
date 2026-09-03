772aa8b1326e5c79cbf8b814963e6586f98e3d97
nxstray <afwanapriansyah@gmail.com>
2026-09-04 01:14:26 +0700

    feat: add MoonIcon component

---
+++ src/components/icons/MoonIcon.vue
@@ -0,0 +1,4 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" :width="size" :height="size" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" :class="className"><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"/></svg>
+</template>
+<script setup>defineProps({ size: { type: [String, Number], default: 24 }, className: { type: String, default: '' } })</script>
