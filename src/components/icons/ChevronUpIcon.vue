5b425ded54f300b75c87403b85c790a0acf7ae4f
nxstray <afwanapriansyah@gmail.com>
2026-09-08 00:16:27 +0700

    feat: add ChevronUpIcon component

---
+++ src/components/icons/ChevronUpIcon.vue
@@ -0,0 +1,8 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" :width="size" :height="size" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" :class="className">
+    <polyline points="18 15 12 9 6 15"/>
+  </svg>
+</template>
+<script setup>
+defineProps({ size: { type: [String, Number], default: 24 }, className: { type: String, default: '' } })
+</script>
