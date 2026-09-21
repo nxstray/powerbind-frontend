fcf106b4897e45d6d01e90fe89f4717866593867
nxstray <afwanapriansyah@gmail.com>
2026-09-21 20:25:43 +0700

    refactor(jwt): use replaceAll and codePointAt (S7781, S7758)

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -5,11 +5,11 @@ export function decodeJwtPayload(token) {
   if (!token) return null
   try {
     const base64Url = token.split('.')[1]
-    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
+    const base64 = base64Url.replaceAll('-', '+').replaceAll('_', '/')
     const json = decodeURIComponent(
       atob(base64)
         .split('')
-        .map((c) => '%' + c.charCodeAt(0).toString(16).padStart(2, '0'))
+        .map((c) => '%' + c.codePointAt(0).toString(16).padStart(2, '0'))
         .join(''),
     )
     return JSON.parse(json)
