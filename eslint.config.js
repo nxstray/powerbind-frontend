63f4422847722685065316235bebe1b61ad12fbd
nxstray <afwanapriansyah@gmail.com>
2026-09-26 22:20:18 +0700

    build(lint): disable multi-word component name rule for ui primitives

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,12 @@ export default defineConfig([
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
 
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+    },
+  },
+
   ...pluginOxlint.buildFromOxlintConfigFile('.oxlintrc.json'),
 
   skipFormatting,
